ecf6201fb60a210dfcbf4175547190f574132b31
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-09-30 11:11:43 -0400

    Commit

---
+++ src/main/java/com/pluralsight/hello.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class hello {
+    
+    public static void main(String[] args) {
+        sayHi():
+    }
+
+    private static void sayHi() {
+    }
+
+    public static void sayHI(){
+        System.out.println("HI");
+    }
+}
+
+
+
